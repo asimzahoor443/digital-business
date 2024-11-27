68e4d843def3500732098b506f25494e9b22f241
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-11-27 12:11:53 +0500

    initial updates

--- src/Components/EmailBox/EmailBox.jsx
+++ src/Components/EmailBox/EmailBox.jsx
@@ -1,20 +1,62 @@
 import React from 'react';
 import './EmailBox.css';
+import { motion } from 'framer-motion';
 import { LuMail } from 'react-icons/lu';
+import { containerVariants } from '@/src/utils/animation';
 const EmailBox = () => {
   return (
-    <div className="emailBox">
+    <motion.div
+      initial={{
+        width: '5rem',
+        borderRadius: '100%',
+      }}
+      whileInView={{
+        width: '70%',
+        borderRadius: '999px',
+        transition: {
+          type: 'easeOut',
+          duration: 1,
+        },
+      }}
+      className="emailBox"
+    >
       {/* ICON */}
-      <div>
+      <motion.div
+        variants={containerVariants(0.6)}
+        initial="offscreen"
+        whileInView={'onscreen'}
+        viewport={{
+          once: true,
+        }}
+      >
         <LuMail size={30} color="gray" />
-      </div>
+      </motion.div>
 
       {/* Input */}
-      <input type="email" placeholder="Enter Email" />
+      <motion.input
+        variants={containerVariants(0.7)}
+        initial="offscreen"
+        whileInView={'onscreen'}
+        viewport={{
+          once: true,
+        }}
+        type="email"
+        placeholder="Enter Email"
+      />
 
       {/* Get Funded Button*/}
-      <div className="getFunded">Get Funded</div>
-    </div>
+      <motion.div
+        variants={containerVariants(0.9)}
+        initial="offscreen"
+        whileInView={'onscreen'}
+        viewport={{
+          once: true,
+        }}
+        className="getFunded"
+      >
+        Get Funded
+      </motion.div>
+    </motion.div>
   );
 };
 
