94cf5651d6185a99dca6d36b2f31f93aa0c0f025
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-11-28 15:20:03 +0500

    updates

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Navbar from '@/src/Components/Navbar/Navbar';
 import './page.css';
 import Hero from '@/src/Components/Hero/Hero';
+import BrandingVideo from '@/src/Components/BrandingVideo/BrandingVideo';
 export default function Home() {
   return (
     <div className="app">
       <Navbar />
       <Hero />
+      <BrandingVideo />
     </div>
   );
 }
---
+++ src/Components/WhatWeDo/WhatWeDo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './WhatWeDo.css';
+const WhatWeDo = () => {
+  return <div>WhatWeDo</div>;
+};
+
+export default WhatWeDo;
