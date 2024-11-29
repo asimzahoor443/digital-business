8465db5ecfcebe1f2d9bb93c4bb9123231608992
Asim Zahoor <113241389+asimzahoor443@users.noreply.github.com>
2024-11-29 20:37:30 +0500

    updates

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+'use client';
 import Navbar from '@/src/Components/Navbar/Navbar';
 import './page.css';
 import Hero from '@/src/Components/Hero/Hero';
@@ -6,13 +7,29 @@ import WhatWeDo from '@/src/Components/WhatWeDo/WhatWeDo';
 import OurDiff from '@/src/Components/OurDiff/OurDiff';
 import { motion, useAnimation } from 'framer-motion';
 export default function Home() {
+  const controls = useAnimation();
   return (
-    <div className="app">
+    <motion.div className="app" animate={controls}>
       <Navbar />
       <Hero />
       <BrandingVideo />
       <WhatWeDo />
-      <OurDiff />
-    </div>
+
+      <motion.div
+        onViewportEnter={() =>
+          controls.start({
+            backgroundColor: 'var(--secondary-color)',
+          })
+        }
+        onViewportLeave={() =>
+          controls.start({
+            background: 'white',
+          })
+        }
+        viewport={{ amount: 0.4 }}
+      >
+        <OurDiff />
+      </motion.div>
+    </motion.div>
   );
 }
--- src/Components/OurDiff/OurDiff.jsx
+++ src/Components/OurDiff/OurDiff.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import './OurDiff.css';
 const OurDiff = () => {
-  return <div className="od-wrapper">Our difference component</div>;
+  return (
+    <div className="od-wrapper">
+      <div className="container">
+        <div className="od-container">
+          {/* Head */}
+          <div className="od-head">
+            <span className="tag">Our Difference</span>
+            <span className="title">Fair capital, hassle free</span>
+            <span className="text">
+              Our mission is to level the playing field for early stage growth
+              capital. We provide capital that is unbiased, flexible and non
+              dilutive with the execution support to accelerate value creation.
+            </span>
+          </div>
+
+          {/* Feature */}
+          <div className="od-features">
+            {
+              
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default OurDiff;
